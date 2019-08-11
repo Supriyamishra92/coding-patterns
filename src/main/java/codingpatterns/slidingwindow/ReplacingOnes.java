2b3fd9ed6276f02bb9c4e6c012a1fee3868cc3c3
Supriya Mishra <supriyamishra05992@gmail.com>
2019-08-12 03:59:51 +0530

    added sliding window replacingOnes problem of window-slider patterns problem

--- src/main/java/codingpatterns/slidingwindow/ReplacingOnes.java
+++ src/main/java/codingpatterns/slidingwindow/ReplacingOnes.java
@@ -1,4 +1,49 @@
 package codingpatterns.slidingwindow;
-
+/***
+ *
+ * PS : Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s,
+ * find the length of the longest subarray having all 1s.
+ *
+ * Input: Array=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2
+ * Output: 6
+ * Explanation: Replace the '0' at index 5 and 8 to have the longest subarray of 1s having length 6.
+ */
 public class ReplacingOnes {
+
+    private static int maxOnesCount(int[] array, int k ) {
+        int max = 0;
+        int windowStart =0;
+        int maxOnesCount =0;
+
+        for (int windowEnd = 0; windowEnd < array.length ; windowEnd++) {
+            if (array[windowEnd] == 1) {
+                maxOnesCount++;
+            }
+
+//             current window size is from windowStart to windowEnd, overall we have a maximum of 1s
+//             repeating a maximum of 'maxOnesCount' times, this means that we can have a window with
+//             'maxOnesCount' 1s and the remaining are 0s which should replace with 1s.
+//             now, if the remaining 0s are more than 'k', it is the time to shrink the window as we
+//             are not allowed to replace more than 'k' Os
+
+            if (windowEnd - windowStart + 1 - maxOnesCount > k) {
+                if (array[windowStart] == 1 ) {
+                    maxOnesCount--;
+                }
+                windowStart++;
+            }
+
+            max = Math.max(max, windowEnd - windowStart+1);
+
+        }
+
+
+        return max;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(ReplacingOnes.maxOnesCount(new int[] { 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1 }, 2));
+        System.out.println(ReplacingOnes.maxOnesCount(new int[] { 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1 }, 3));
+    }
+
 }
--- src/main/java/codingpatterns/slidingwindow/ReplacingOnesBrute.java
+++ src/main/java/codingpatterns/slidingwindow/ReplacingOnesBrute.java
@@ -1,5 +1,7 @@
 package codingpatterns.slidingwindow;
 
+import java.util.Stack;
+
 /***
  *
  * PS : Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s,
@@ -8,6 +10,40 @@ package codingpatterns.slidingwindow;
  * Input: Array=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2
  * Output: 6
  * Explanation: Replace the '0' at index 5 and 8 to have the longest subarray of 1s having length 6.
+ *
+ *
  */
 public class ReplacingOnesBrute {
+
+    private static int maximizeOnes(int[] array, int k) {
+
+        //0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            int ones = 0;
+            int chance = 0;
+            for (int j = i; j < array.length ; j++) {
+                   if (array[j] == 0 ) {
+                       if (chance < k) {
+                           chance++;
+                           ones++;
+                       } else {
+                           break;
+                       }
+                   } else {
+                       ones++;
+                   }
+            }
+        max = Math.max(ones, max);
+        }
+       return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ReplacingOnesBrute.maximizeOnes(new int[] { 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1 }, 2));
+        System.out.println(ReplacingOnesBrute.maximizeOnes(new int[] { 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1 }, 3));
+    }
+
+
+
 }
